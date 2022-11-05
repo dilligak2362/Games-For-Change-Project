0452cc54c2732f354ee86eca4ea58ca1a19e6753
Kylen Dilligard <kidf6m3@gmail.com>
2022-11-05 17:12:52 -0500

    Interaction System

---
+++ Games For Change Project/Assets/Scripts/InteractionSystem.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class InteractionSystem : MonoBehaviour
+{
+    //Detection Point
+    public Transform detectionPoint;
+    //Detection Radius
+    private const float detectionRadius = 0.2f;
+    //Detection Layer
+    public LayerMask detectionLayer;
+    //Cached Trigger Object
+    public GameObject detectedObject;
+
+    void Update()
+    {
+        if (DetectObject())
+        {
+            if (InteractInput())
+            {
+                Debug.Log("INTERACT");
+                detectedObject.GetComponent<Item>().Interact();
+            }
+        }
+    }
+
+    bool InteractInput()
+    {
+        return Input.GetKeyDown(KeyCode.E);
+
+    }
+
+    bool DetectObject()
+    {
+
+        Collider2D obj = Physics2D.OverlapCircle(detectionPoint.position, detectionRadius, detectionLayer);
+
+        if (obj == null)
+        {
+            detectedObject = null;
+            return false;
+        }
+        else
+        {
+            detectedObject = obj.gameObject;
+            return true;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawSphere(detectionPoint.position, detectionRadius);
+    }
+}
---
+++ Games For Change Project/Assets/Scripts/Item.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class Item : MonoBehaviour
+{
+    public enum InteractionType { NONE,PickUp,Examine}
+    public InteractionType type;
+   
+    private void Reset()
+    {
+        GetComponent<Collider2D>().isTrigger = true;
+        gameObject.layer = 10; 
+
+    }
+
+    public void Interact()
+    {
+        switch(type)
+        {
+            case InteractionType.PickUp:
+                Debug.Log("PICK UP");
+                break;
+            case InteractionType.Examine:
+                Debug.Log("PICK UP");
+                break;
+            default:
+                Debug.Log("NULL ITEM");
+                break; 
+        }
+
+    }
+}
