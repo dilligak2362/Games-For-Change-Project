9529f46b580821ccf2b6696e91f740a03a4f935a
Kylen Dilligard <kidf6m3@gmail.com>
2022-11-06 12:03:03 -0600

    Camera Follow Script

---
+++ Games For Change Project/Assets/CameraFollow.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public float FollowSpeed = 2f;
+    public float yOffset = 1f;
+    public Transform target; 
+
+    void Update()
+    {
+        Vector3 newPos = new Vector3(target.position.x, target.position.y + yOffset, -10f);
+        transform.position = Vector3.Slerp(transform.position, newPos, FollowSpeed * Time.deltaTime);
+    }
+}
