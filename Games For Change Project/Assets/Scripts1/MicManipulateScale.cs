8bfac1d35d698d80c249ef48d4773dbd5e730ebe
EshaM3 <90992413+EshaM3@users.noreply.github.com>
2022-11-08 02:32:58 -0500

    SceneManager Development
    I worked on the overall scene manager to conduct how the mic input will go, adding in a timer. I also added more to the comment plan to flesh out how I will finish up and merge this branch once i add the dialogue feature.

--- Games For Change Project/Assets/Scripts1/MicManipulateScale.cs
+++ Games For Change Project/Assets/Scripts1/MicManipulateScale.cs
@@ -8,6 +8,8 @@ public class MicManipulateScale : MonoBehaviour
     public Vector2 minScale;
     public Vector2 maxScale;
     public MicInput detector;
+    public float loudness;
+    public Vector2 loudnessLerp;
 
     public float volumeThreshold = 0.03f;
     public float sensitivity = 100;
@@ -23,11 +25,11 @@ public class MicManipulateScale : MonoBehaviour
     {
         //float loudness = detector.GetLoudnessFromAudioClip(source.timeSamples, source.clip); - this is for audio clips
 
-        float loudness = detector.GetLoudnessFromMicrophoneClip()*sensitivity;
+        loudness = detector.GetLoudnessFromMicrophoneClip()*sensitivity;
 
         if (loudness < volumeThreshold) loudness = 0;
 
         //lerp will confine the loudness value between chosen minScale and maxScale values
-        transform.localScale = Vector2.Lerp(minScale, maxScale, loudness);
+        loudnessLerp = transform.localScale = Vector2.Lerp(minScale, maxScale, loudness);
     }
 }
--- Games For Change Project/Assets/Scripts1/SceneManager.cs
+++ Games For Change Project/Assets/Scripts1/SceneManager.cs
@@ -1,34 +1,96 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class SceneManager : MonoBehaviour
 {
+    public bool dialogueEnded; //used for the interaction script to know when to unfreeze character; that script should then set this bool to false
+    public bool useFragments; //used for the interaction script to know when to drop the soul fragment count; that script should then set this bool to false
+    private bool micStarted;
+    private bool volumeFirstReached;
+    public TextMeshProUGUI micInstructions;
+    public GameObject volumeBar;
+    public TextMeshProUGUI micTimer;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        ActivateMicInstructions();  
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(micStarted == true)
+        {
+            if (volumeBar.transform.GetChild(0).gameObject.GetComponent<MicManipulateScale>().loudnessLerp.y >= 0.8f && !volumeFirstReached)
+            {
+                StartCoroutine(Timer());
+            }
+            else if (volumeBar.transform.GetChild(0).gameObject.GetComponent<MicManipulateScale>().loudnessLerp.y < 0.6f && volumeFirstReached)
+            {
+                StopCoroutine(Timer());
+                micTimer.gameObject.SetActive(false);
+                volumeFirstReached = false;
+            }
+        }
     }
 
+    IEnumerator InitialInstructions()
+    {
+        micInstructions.gameObject.SetActive(true);
+        yield return new WaitForSeconds(3);
+        micInstructions.gameObject.SetActive(false);
+        volumeBar.gameObject.SetActive(true);
+        micStarted = true;
+    }
+
+    IEnumerator Timer()
+    {
+        volumeFirstReached = true;
+        micTimer.gameObject.SetActive(true);
+        micTimer.text = "Hold for 3";
+        yield return new WaitForSeconds(1);
+        micTimer.text = "Hold for 2";
+        yield return new WaitForSeconds(1);
+        micTimer.text = "Hold for 1";
+        yield return new WaitForSeconds(1);
+        micStarted = false;
+        micTimer.gameObject.SetActive(false);
+
+        //initiate the soul fragment animation #1 here
+
+        useFragments = true;
+        volumeBar.SetActive(false);
+
+        //activate dialogue box...
+    }
+    
+
+    public void ActivateMicInstructions() //used for the interaction script to call
+    {
+        StartCoroutine(InitialInstructions());  
+    }
+
+
+
     /*
-     * freezes character in place (or this can maybe be done when you interact in the interact script
+     * freezes character in place (or this can maybe be done when you interact in the interact script)
+     * have interact script set dialogue ended bool as false; do the activate mic instructions and keep player frozen until dialogue ended bool is true
      * activates and deactivates text that will say "Sing into your mic to fill up the voice-to-soul meter!"
      * activates mic-manipulated object
-     * using mic-manipulated object; if it's a certain size, trigger soul fragment animation** (#1) 
+     * using mic-manipulated object; if it's a certain size, trigger counter for 3 seconds on screen, then trigger soul fragment animation** (#1) 
      * and drop soul fragment count down to 0
-     * reset mic-manipulated object to original parameters and deactivate it
+     * deactivate mic-manipulated object
      * activate dialogue box and text with dialogue script that will start on its own and
      * continue with user input
      * after dialogue's while loop is done, it will change a boolean that the manager will keep
      * track of in update; this will allow us to close the dialogue box and trigger an animation
      * where the soul is disintegrated - animation** (#2)
      * deactivate the soul
-     * tells the interact script somehow to unfreeze the player's movements
+     * tells the interact script to unfreeze the player's movements - dialogue ended bool is true now
      */
 }
